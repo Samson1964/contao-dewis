912f4c0de02374ea1bccc34a21373a3e29a799dc
Frank Hoppe <hoppe.berlin@googlemail.com>
2017-07-14 10:17:49 +0200

    Übersetzung FE-Module hinzugefügt

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -15,3 +15,12 @@ $GLOBALS['TL_LANG']['MOD']['dwz-spieler'] = array('Spieler', 'Spieler verwalten'
 $GLOBALS['TL_LANG']['MOD']['dwz-vereine'] = array('Vereine', 'Vereine verwalten');
 $GLOBALS['TL_LANG']['MOD']['dwz-turniere'] = array('Turniere', 'Turniere verwalten');
 $GLOBALS['TL_LANG']['MOD']['dwz-bearbeiter'] = array('Bearbeiter', 'Bearbeiter verwalten');
+
+/**
+ * Frontendmodule
+ */
+$GLOBALS['TL_LANG']['FMD']['dewis'] = 'DeWIS';
+$GLOBALS['TL_LANG']['FMD']['dewis_spieler'] = array('Spielersuche','Spielersuche und -ausgabe aus DeWIS');
+$GLOBALS['TL_LANG']['FMD']['dewis_verein'] = array('Vereinssuche','Vereinssuche und -ausgabe aus DeWIS');
+$GLOBALS['TL_LANG']['FMD']['dewis_verband'] = array('Verbandssuche','Verbandssuche und -ausgabe aus DeWIS');
+$GLOBALS['TL_LANG']['FMD']['dewis_turnier'] = array('Turniersuche','Turniersuche und -ausgabe aus DeWIS');
